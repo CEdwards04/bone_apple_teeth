2d5dbffcfe69369d2ec9ed5a46b6a714c5ce57da
DatManJer <jka00003@mix.wvu.edu>
2024-02-29 15:20:18 -0500

    got the image working in the navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,22 @@
+import logo from './Bone_Apple_Teeth-logos_transparent.png';
+
 function Navbar() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-            <img src="Bone_Apple_Teeth-logos_transparent.png" alt="Bone Apple Teeth Logo"/>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-link" href="#">About</a>
-                    <a className="nav-link" href="#">Contact</a>
-                </div>
-            </div>
-            <a className="nav-link" href="#"><button className="btn btn-dark" type="submit">Login/Register</button></a>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand">
+          <img src={logo} alt="Bone Apple Teeth Logo" style={{ width: '100px', height: 'auto' }} />
+        </a>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link" href="#">About</a>
+            <a className="nav-link" href="#">Contact</a>
+          </div>
         </div>
-      </nav>
-    );
-  }
-  
-  export default Navbar;
+        <a className="nav-link" href="#"><button className="btn btn-dark" type="submit">Login/Register</button></a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
