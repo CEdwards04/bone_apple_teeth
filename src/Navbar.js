4a1f8aca5684c4dd8bcbccbc9af9ebce486fa1c0
jta00007 <jta00007@mix.wvu.edu>
2024-02-28 19:19:50 -0500

    Added Bootstrap

---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar() {
+    return (
+      <div className="App">
+            <h1>CS 230 Group 8 Project</h1>
+       <h2>Bone Apple Teeth</h2>
+      </div>
+    );
+  }
+  
+  export default Navbar;
