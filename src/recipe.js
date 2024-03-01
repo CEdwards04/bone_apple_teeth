418a9d324d5515e94637d9698e95a3ab00878269
actionjacks0n <jss00023@mix.wvu.edu>
2024-03-01 18:35:28 -0500

    Created and added some code to recipe.css and recipe.js

---
+++ src/recipe.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './recipe.css';
+import './Navbar';
+import Navbar from './Navbar';
+
+function Recipe() {
+  return (
+    <div className="recipe">
+        <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default Recipe;
