613f60aa3df4939d3af4a4d1a5fa92e52df68121
jta00007 <jta00007@mix.wvu.edu>
2024-03-28 02:32:55 -0400

    Attempt Number 3 at login functionality

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import LoginPage from './LoginPage';
 import ProfilePopup from './Pages/ProfilePage/ProfilePopup';
 import UserProfile from './Pages/ProfilePage/ProfilePage';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <BrowserRouter>
@@ -23,4 +23,22 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
+
+import { Amplify } from 'aws-amplify';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
+
+function App({ signOut, user }) {
+  return (
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
+  );
+}
+
+export default withAuthenticator(App);
