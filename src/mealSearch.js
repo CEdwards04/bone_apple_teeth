22f03216df3507965bc24cf6e862e046567e2190
DatManJer <jka00003@mix.wvu.edu>
2024-03-31 18:45:45 -0400

    api work

--- src/mealSearch.js
+++ src/mealSearch.js
@@ -1,28 +1,87 @@
-import './App.css';
-import './Navbar';
+import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import './Pages/ProfilePage/ProfilePopup';
 import './mealCard';
 import Card from './mealCard';
 import mealSearchStyle from './CSS Modules/mealSearch.module.css';
 import MealSearchFunction from './mealSearchFunction';
+import './style.css';
 
 function MealSearch() {
+    const [recipeData, setRecipeData] = useState(null);
+
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            const analyzeUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/analyze?language=en&includeNutrition=false&includeTaste=false';
+            const analyzeOptions = {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                    'X-RapidAPI-Key': '42bae32b8cmsh6a99cae29fbd3e3p154198jsn4da3a35b7cd5',
+                    'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+                },
+                body: JSON.stringify({
+                    title: 'Spaghetti Carbonara',
+                    servings: 2,
+                    ingredients: [
+                        '1 lb spaghetti',
+                        '3.5 oz pancetta',
+                        '2 Tbsps olive oil',
+                        '1  egg',
+                        '0.5 cup parmesan cheese'
+                    ],
+                    instructions: 'Bring a large pot of water to a boil and season generously with salt. Add the pasta to the water once boiling and cook until al dente. Reserve 2 cups of cooking water and drain the pasta. '
+                })
+            };
+
+            try {
+                const response = await fetch(analyzeUrl, analyzeOptions);
+                const result = await response.text();
+                console.log(result); // Optionally, log the result
+            } catch (error) {
+                console.error(error);
+            }
+
+            const searchUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=apples%2Cflour%2Csugar&number=5&ignorePantry=true&ranking=1';
+            const searchOptions = {
+                method: 'GET',
+                headers: {
+                    'X-RapidAPI-Key': '42bae32b8cmsh6a99cae29fbd3e3p154198jsn4da3a35b7cd5',
+                    'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+                }
+            };
+
+            try {
+                const response = await fetch(searchUrl, searchOptions);
+                const result = await response.json();
+                setRecipeData(result);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchRecipes();
+    }, []);
+
     return (
         <div>
-          <Navbar></Navbar>
-          <h1>Meal Search</h1>
-          <h2>Bone Apple Teeth</h2>
-          <table>
-            <tr>
-              <td>
-                <MealSearchFunction></MealSearchFunction>
-              </td>
-              <td className={mealSearchStyle.meal_cards}><Card></Card></td>
-            </tr>
-          </table>
+            <Navbar />
+            <h1>Meal Search</h1>
+            <h2>Bone Apple Teeth</h2>
+            <table>
+                <tr>
+                    <td>
+                        <MealSearchFunction />
+                    </td>
+                    <td className={mealSearchStyle.meal_cards}>
+                        {recipeData && recipeData.map((recipe, index) => (
+                            <Card key={index} recipe={recipe} />
+                        ))}
+                    </td>
+                </tr>
+            </table>
         </div>
-      );
+    );
 }
 
 export default MealSearch;
--- src/mealSearchFunction.jsx
+++ src/mealSearchFunction.jsx
@@ -1,147 +1,135 @@
 function MealSearchFunction() {
     return (
         <form>
-            <label for="force_only_selected">Force only selected </label>
-            <input type="checkbox" id="force_only_selected" name="force_only_selected"></input>
-            <br></br>
-            <label>Dairy</label>
-            <label for="test">Milk </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Butter </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Cheese </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Yogurt </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Ice Cream </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test1">Test1 </label>
-            
-            <label>Grains</label>
-            <label for="test">Bread </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Oats </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Oats </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-
-            <label>Whole Grains</label>
-            <label for="test">Barley </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Rice </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-
-            <label>Fruits</label>
-            <label for="test">Apples </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Bananas </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Oranges </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Lemons </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Limes </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Peaches </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Pears </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Pineapples </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Strawberries </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Blueberries </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Raspberries </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-
-            <label>Meats</label>
-            <label for="test">Beef </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Pork </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Lamb </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Veal </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Milk </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-
-            <label>Poultry</label>
-            <label for="test">Chicken </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Turkey </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Duck </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-
-            <label>Fish/Seafood</label>
-            <label for="test">Fish </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Scallops </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Fish </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Prawns </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Lobster </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Oysters </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Clams </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            
-            <label>Oils</label>
-            <label for="test">Olive Oil </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Coconut Oil </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Avocado Oil </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
-            <label for="test">Vegetable Oil </label>
-            <input type="checkbox" id="dairy" name="dairy"></input>
-            <br></br>
+            <label htmlFor="force_only_selected">Force only selected </label>
+            <input type="checkbox" id="force_only_selected" name="force_only_selected" />
+            <br />
+            <label htmlFor="milk">Milk </label>
+            <input type="checkbox" id="milk" name="dairy" />
+            <br />
+            <label htmlFor="butter">Butter </label>
+            <input type="checkbox" id="butter" name="dairy" />
+            <br />
+            <label htmlFor="cheese">Cheese </label>
+            <input type="checkbox" id="cheese" name="dairy" />
+            <br />
+            <label htmlFor="yogurt">Yogurt </label>
+            <input type="checkbox" id="yogurt" name="dairy" />
+            <br />
+            <label htmlFor="ice_cream">Ice Cream </label>
+            <input type="checkbox" id="ice_cream" name="dairy" />
+            <br />
+            <label htmlFor="test1">Test1 </label>
+            <input type="checkbox" id="test1" name="test1" />
+            <br />
+            <label htmlFor="bread">Bread </label>
+            <input type="checkbox" id="bread" name="grains" />
+            <br />
+            <label htmlFor="oats1">Oats </label>
+            <input type="checkbox" id="oats1" name="grains" />
+            <br />
+            <label htmlFor="oats2">Oats </label>
+            <input type="checkbox" id="oats2" name="grains" />
+            <br />
+            <label htmlFor="barley">Barley </label>
+            <input type="checkbox" id="barley" name="whole_grains" />
+            <br />
+            <label htmlFor="rice">Rice </label>
+            <input type="checkbox" id="rice" name="whole_grains" />
+            <br />
+            <label htmlFor="apples">Apples </label>
+            <input type="checkbox" id="apples" name="fruits" />
+            <br />
+            <label htmlFor="bananas">Bananas </label>
+            <input type="checkbox" id="bananas" name="fruits" />
+            <br />
+            <label htmlFor="oranges">Oranges </label>
+            <input type="checkbox" id="oranges" name="fruits" />
+            <br />
+            <label htmlFor="lemons">Lemons </label>
+            <input type="checkbox" id="lemons" name="fruits" />
+            <br />
+            <label htmlFor="limes">Limes </label>
+            <input type="checkbox" id="limes" name="fruits" />
+            <br />
+            <label htmlFor="peaches">Peaches </label>
+            <input type="checkbox" id="peaches" name="fruits" />
+            <br />
+            <label htmlFor="pears">Pears </label>
+            <input type="checkbox" id="pears" name="fruits" />
+            <br />
+            <label htmlFor="pineapples">Pineapples </label>
+            <input type="checkbox" id="pineapples" name="fruits" />
+            <br />
+            <label htmlFor="strawberries">Strawberries </label>
+            <input type="checkbox" id="strawberries" name="fruits" />
+            <br />
+            <label htmlFor="blueberries">Blueberries </label>
+            <input type="checkbox" id="blueberries" name="fruits" />
+            <br />
+            <label htmlFor="raspberries">Raspberries </label>
+            <input type="checkbox" id="raspberries" name="fruits" />
+            <br />
+            <label htmlFor="beef">Beef </label>
+            <input type="checkbox" id="beef" name="meats" />
+            <br />
+            <label htmlFor="pork">Pork </label>
+            <input type="checkbox" id="pork" name="meats" />
+            <br />
+            <label htmlFor="lamb">Lamb </label>
+            <input type="checkbox" id="lamb" name="meats" />
+            <br />
+            <label htmlFor="veal">Veal </label>
+            <input type="checkbox" id="veal" name="meats" />
+            <br />
+            <label htmlFor="meat_milk">Milk </label>
+            <input type="checkbox" id="meat_milk" name="meats" />
+            <br />
+            <label htmlFor="chicken">Chicken </label>
+            <input type="checkbox" id="chicken" name="poultry" />
+            <br />
+            <label htmlFor="turkey">Turkey </label>
+            <input type="checkbox" id="turkey" name="poultry" />
+            <br />
+            <label htmlFor="duck">Duck </label>
+            <input type="checkbox" id="duck" name="poultry" />
+            <br />
+            <label htmlFor="fish">Fish </label>
+            <input type="checkbox" id="fish" name="fish_seafood" />
+            <br />
+            <label htmlFor="scallops">Scallops </label>
+            <input type="checkbox" id="scallops" name="fish_seafood" />
+            <br />
+            <label htmlFor="fish2">Fish </label>
+            <input type="checkbox" id="fish2" name="fish_seafood" />
+            <br />
+            <label htmlFor="prawns">Prawns </label>
+            <input type="checkbox" id="prawns" name="fish_seafood" />
+            <br />
+            <label htmlFor="lobster">Lobster </label>
+            <input type="checkbox" id="lobster" name="fish_seafood" />
+            <br />
+            <label htmlFor="oysters">Oysters </label>
+            <input type="checkbox" id="oysters" name="fish_seafood" />
+            <br />
+            <label htmlFor="clams">Clams </label>
+            <input type="checkbox" id="clams" name="fish_seafood" />
+            <br />
+            <label htmlFor="olive_oil">Olive Oil </label>
+            <input type="checkbox" id="olive_oil" name="oils" />
+            <br />
+            <label htmlFor="coconut_oil">Coconut Oil </label>
+            <input type="checkbox" id="coconut_oil" name="oils" />
+            <br />
+            <label htmlFor="avocado_oil">Avocado Oil </label>
+            <input type="checkbox" id="avocado_oil" name="oils" />
+            <br />
+            <label htmlFor="vegetable_oil">Vegetable Oil </label>
+            <input type="checkbox" id="vegetable_oil" name="oils" />
+            <br />
         </form>
     );
 }
 
 export default MealSearchFunction;
+
