40dd26ba5a87d291fb30012c4e72aed1455352d2
CEdwards04 <cmedwards2004@gmail.com>
2024-03-31 14:49:51 -0400

    added graphql api and basic create update and dele te functions in app.js that we can modify

--- src/App.js
+++ src/App.js
@@ -7,15 +7,61 @@ import MealSearch from './mealSearch';
 import LoginPage from './LoginPage';
 import ProfilePopup from './Pages/ProfilePage/ProfilePopup';
 import UserProfile from './Pages/ProfilePage/ProfilePage';
-
+import { createTodo, updateTodo, deleteTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
 import { Amplify } from 'aws-amplify';
-
+import { generateClient } from 'aws-amplify/api';
+import config from './amplifyconfiguration.json';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import config from './amplifyconfiguration.json';
+import { createElement, useEffect } from 'react';
 Amplify.configure(config);
 
 function App({ signOut, user }) {
+  
+  useEffect(() => {
+  const client = generateClient();
+ 
+async function createTodoItem(){
+const createResult = await client.graphql({
+  query: createTodo,
+  variables: {
+    input: {
+      name: 'My first todo!'
+    }
+  }
+});
+};
+
+
+async function updateTodoItem(){
+const updateResult = await client.graphql({
+  query: updateTodo,
+  variables: {
+    input: {
+      id: '<...>',
+      name: 'My first updated todo!'
+    }
+  }
+});
+console.log(updateResult);
+};
+
+
+async function deleteTodoItem(){
+const deleteResult = await client.graphql({
+  query: deleteTodo,
+  variables: {
+    input: {
+      id: '<...>'
+    }
+  }
+});
+console.log(deleteResult);
+}
+
+  }, []);
+  
   return (
     <div className="App">
       <button onClick={signOut}>Sign out</button>
@@ -31,7 +77,6 @@ function App({ signOut, user }) {
 }
 
 export default withAuthenticator(App);
-
 
 /*function App() {
   return (
