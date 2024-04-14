a0de49dc83417fef2ee83273f1f42f3c3c6a149b
jta00007 <jta00007@mix.wvu.edu>
2024-04-13 23:35:13 -0400

    My side of updated code from amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,6 +3,7 @@ export type AmplifyDependentResourcesAttributes = {
     "boneappleteeth7e2ffc44": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
       "UserPoolArn": "string",
