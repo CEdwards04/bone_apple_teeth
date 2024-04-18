bd6f3171837394321b72aeea4f57ddf76744c4cb
jta00007 <jta00007@mix.wvu.edu>
2024-04-18 12:09:57 -0400

    Updated mealCard to not display titles beside cards

--- src/mealCard.js
+++ src/mealCard.js
@@ -10,7 +10,6 @@
 function Card({ recipe }) {
     return (
         <div className="d-flex justify-content-center">
-            <h1>{recipe.title}</h1>
             <div className="row row-cols-1 row-cols-md-4 g-4">
                 <div className="col">
                     <div className="card text-bg-secondary" style={{width: '18rem'}}>
